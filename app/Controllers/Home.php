f1f84594ef79de6b8a23d4cd42b431841cc9d7c0
Ika rahma alia <ika418471@gmail.com>
2023-09-15 07:58:55 +0700

    profile

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,4 +8,30 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
+
+    // public function profile()
+    // {
+    //     #return view('profile');
+    //     $data = [
+    //     'nama' => 'Ika Rahma Alia',
+    //     'npm' => '2117051016',
+    //     'alamat' => 'Lampung Barat'
+    //     ];
+
+    //     return view('profile', $data);
+    // }
+
+    public function profile($nama = "Ika Rahma Alia", $kelas = "CD", $npm = "2117051016", $alamat = "Lampung Barat")
+    {
+        $data = [
+            'nama' => $nama,
+            'kelas' => $kelas,
+            'npm' => $npm,
+            'alamat' => $alamat,
+        ];
+        return view('profile', $data);
+    }
+        
+
+    
 }
